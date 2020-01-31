31660bfa1d1b6376239169c80cc9851d09128ee1
FadelMohamad370 <fadelalmasyhur09@gmail.com>
2020-01-31 08:54:39 +0800

    Aku_Pintar

---
+++ app/src/main/java/com/fadelmohamad/akupintar/MainActivity.java
@@ -0,0 +1,39 @@
+package com.fadelmohamad.akupintar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        iv = (ImageView) findViewById(R.id.iv);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.mytransiton);
+        iv.startAnimation(myanim);
+        final Intent i = new Intent(this, HalamanUtamaActivity.class);
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
+
+
